a890186f956fb0cac1af13d66bd056f54533aef2
Deeksha Namani <dnamani1@my.westga.edu>
2024-04-29 01:13:40 -0400

    Added mockup, class and start diagram.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,24 +2,6 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show ]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
   # POST /comments or /comments.json
   def create
     @restaurant = Restaurant.find_by(id: params[:comment][:restaurant_id])
@@ -42,29 +24,6 @@ class CommentsController < ApplicationController
       format.json { render json: @comment.errors, status: :unprocessable_entity }
     end
    end
-  end
-
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully updated." }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
